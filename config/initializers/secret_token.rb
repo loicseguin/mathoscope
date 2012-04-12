9fe7aa7d8beb71e06fa09ebc01c885e8b03d2621
Loïc Séguin-C <loicseguin@gmail.com>
2012-04-12 18:11:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathoscope::Application.config.secret_token = 'ca63669b6ab8e9f387ef337e56549c97e0eb9ca907c60f8838ffd84b6ede903d60b8a205cf25bd0b006c025eeacfb62c731830e0cf7483234e945ce78e8fa453'
